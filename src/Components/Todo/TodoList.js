a99e0cacfad396f47c7b782387e99d83ba5d2441
Dang Quoc Khanh <khanhdangbmt@gmail.com>
2020-06-18 22:41:44 +0700

    delete ternary operator

--- src/Components/Todo/TodoList.js
+++ src/Components/Todo/TodoList.js
@@ -1,45 +1,51 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Todo from './Todo';
+import React from "react";
+import { connect } from "react-redux";
+import Todo from "./Todo";
 import { checkAllTodos } from "../../store/actions/index";
-import * as Helper from '../Helper/Helper';
+import * as Helper from "../Helper/Helper";
 
-const todoLists = props => {
+const todoLists = (props) => {
   const { todoList, isCheckedAll, checkAllTodos, status } = props;
   const todoLists = Helper.filterTodosByStatus(todoList, status);
-  const displaySelectAll = todoList.length >= 1 ? true : false;
+  const displaySelectAll = todoList.length >= 1;
   return (
     <section className="main">
-      <input className="toggle-all" type="checkbox"
-        onChange={()=>checkAllTodos()}
-        checked={!!isCheckedAll} value=""/>
-      {
-        displaySelectAll ? <label
-        htmlFor="toggle-all"
-        onClick={checkAllTodos}
-      ></label> : ''
-      }
+      <input
+        className="toggle-all"
+        type="checkbox"
+        onChange={() => checkAllTodos()}
+        checked={!!isCheckedAll}
+        value=""
+      />
+      {displaySelectAll && (
+        <label htmlFor="toggle-all" onClick={checkAllTodos}></label>
+      )}
       <ul className="todo-list">
-        {
-          todoLists.map((todo, index) => {
-            return <Todo key={`todo${todo.id}`}  {...{ todo }} {...props} index={index} />
-          })
-        }
+        {todoLists.map((todo, index) => {
+          return (
+            <Todo
+              key={`todo${todo.id}`}
+              {...{ todo }}
+              {...props}
+              index={index}
+            />
+          );
+        })}
       </ul>
     </section>
   );
-}
+};
 
 const mapStateToProps = (state) => {
   return {
     todoList: state.todos.todoList,
     isCheckedAll: state.todos.isCheckedAll,
-    status: state.todos.status
-  }
-}
+    status: state.todos.status,
+  };
+};
 
 const mapDispatchToProps = {
-  checkAllTodos
+  checkAllTodos,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(todoLists);
